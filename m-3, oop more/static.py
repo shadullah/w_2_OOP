43a4078797a087536d537e955a066f677139d848
hexaalif <shadullahsad9@gmail.com>
2023-10-07 01:19:57 +0600

    static method and class method file

---
+++ m-3, oop more/static.py
@@ -0,0 +1,36 @@
+# static attribute (class attribute)
+# static method @staticmethod
+# class method @classmethod
+
+class shopping: 
+    cart =[]
+    origin = 'china'
+
+    def __init__(self,name, location) -> None:
+        self.name = 'syllhet' # instance attribute
+        self.location = 'jam er maj khane'
+
+    def purchase(self, item, price, amount):
+        remaining = amount - price
+        print(f'buying {item} for price:{price} remain {remaining}') 
+
+    @staticmethod
+    def multiply( a,b):
+        res = a*b
+        print(res)
+
+    @classmethod
+    def hudai(self, item):
+        print("hudai dekhi", item)
+
+boshun = shopping('dhara', 'loc')
+# boshun.purchase('lungi', 500, 1000)
+boshun.hudai('lungi')
+# boshun.hudai('lungi')
+shopping.hudai('lungi')
+
+# static method
+shopping.multiply(4,6)
+boshun.multiply(4,6)
+
+# shopping().purchase(2,3,3)
